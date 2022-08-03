675a744e5869b9cc26ee90e4158d06b092878caf
Peder Hovdan Andresen <pederhan@usit.uio.no>
2022-08-03 11:17:59 +0200

    Use jq if possible in test script

--- test.sh
+++ test.sh
@@ -6,38 +6,41 @@ if [ -n "$1" ]; then # if argument 1 is provided, override N_ITERATIONS
     N_ITERATIONS=$1
 fi
 
+JQ_CMD="cat"
+if [ -n "$(command -v jq)" ]; then
+    JQ_CMD="jq -c -j"
+fi
 
 
-
-echo "Testing the currency service"
+echo "Testing the Currency service"
 for (( i=1; i<=$N_ITERATIONS; i++ ))
 do
-    docker exec consul-server1 curl -s http://currency.service.consul/rate/USD?target=ZWL
+    docker exec consul-server1 curl -s http://currency.service.consul/rate/USD?target=ZWL | $JQ_CMD
     echo
 done
 echo
 
-echo "Testing the quotes service"
+echo "Testing the Quotes service"
 for (( i=1; i<=$N_ITERATIONS; i++ ))
 do
-    docker exec consul-server1 curl -s http://quotes.service.consul/
+    docker exec consul-server1 curl -s http://quotes.service.consul/ | $JQ_CMD
     echo
 done
 echo
 
 
-echo "Testing /discover from currency service to quotes service"
+echo "Testing /discover from Currency to Quotes service"
 for (( i=1; i<=$N_ITERATIONS; i++ ))
 do
-    docker exec consul-server1 curl -s http://currency.service.consul/discover
+    docker exec consul-server1 curl -s http://currency.service.consul/discover | $JQ_CMD
     echo
 done
 echo
 
-echo "Testing /discover from currency service to quotes service"
+echo "Testing /discover from Quotes to Currency"
 for (( i=1; i<=$N_ITERATIONS; i++ ))
 do
-    docker exec consul-server1 curl -s http://quotes.service.consul/discover
+    docker exec consul-server1 curl -s http://quotes.service.consul/discover | $JQ_CMD
     echo
 done
 echo
